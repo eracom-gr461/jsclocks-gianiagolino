61b9a2a0b54df18ad55a97c64346d824205af157
gianiagolino <31766872+gianiagolino@users.noreply.github.com>
2017-09-15 15:19:49 +0200

    creation des element

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Détail du brief: https://3.basecamp.com/3165988/buckets/4702925/vaults/65081554
 
 ## Contenu de ce dossier
 
-- index.html = un fichier HTML basique
+-  3index.html = un fichier HTML basique
 
-Ce fichier charge les éléments suivants: 
+Ce fichier charge les éléments suivants:
 
 - css/normalize.css = un "normalisateur", voir http://necolas.github.io/normalize.css/
 - css/style.css = une feuille de style CSS que vous pourrez modifier.
--- js/clocks.js
+++ js/clocks.js
@@ -1,10 +1,12 @@
 
 /*
  * Un petit script mesurant l'écoulement du temps
- * 
+ *
  * Source: https://codepen.io/Tcip/pen/BNKjeN
  * Auteur: Mikael Elmblad, tcip.se
 */
+
+//afficher une alerte hello world
 
 // répéter la fonction metronone une fois par seconde
 
@@ -19,7 +21,7 @@ function metronome() {
   // voir documentation: https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Date
 
   var d = new Date();
-  
+
   // 2: on affiche l'heure dans notre objet "horloge"
 
   document.getElementById("horloge").innerHTML = d.toLocaleTimeString();
