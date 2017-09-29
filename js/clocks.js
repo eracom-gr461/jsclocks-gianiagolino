fdb05d04b0e63506f69a04198adf2af3b2b5215a
gianiagolino <31766872+gianiagolino@users.noreply.github.com>
2017-09-29 15:47:55 +0200

    modifications 29/09

--- js/clocks.js
+++ js/clocks.js
@@ -20,10 +20,35 @@ function metronome() {
   // 1: on définit l'heure, avec l'objet Date()
   // voir documentation: https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Date
 
-  var d = new Date();
+  var date = new Date();
 
-  // 2: on affiche l'heure dans notre objet "horloge"
+  // document.getElementById("horloge").innerHTML = d.toLocaleTimeString();
 
-  document.getElementById("horloge").innerHTML = d.toLocaleTimeString();
+  // mettre les secondes dans le bloc des secondes
+  var seconde = date.getSeconds();
+  var minute = date.getMinutes();
+  var heure = date.getHours();
+
+  document.getElementById("sec-txt").innerHTML = seconde +"s";
+  document.getElementById("min-txt").innerHTML = minute +"m";
+
+  // Faire pencher l'élément des secondes...
+  // transform: skewX(-25deg);
+
+  function SuperMathematique(valeur) {
+    valeur = valeur *1.5;
+    valeur = valeur -45;
+    return valeur;
+  }
+
+  // console.log( SuperMathematique(seconde) );
+
+  document.getElementById("sec-ombre").style.transform = 'skewX('+ SuperMathematique(seconde) +'deg) translateX(0px)';
+
+  document.getElementById("min-ombre").style.transform = 'skewX('+ SuperMathematique(minute) +'deg)';
+
+  // de zéro sec à 60sec = traduire en : de -45 à 45.
+
+
 
 }
